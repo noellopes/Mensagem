31b98aae5a4a262e581dbd4ef70fc295e5cc1a7d
Noel Lopes <noel@ipg.pt>
2021-04-24 16:04:14 +0100

    validar que o utilizador introduziu uma mensagem antes de mudar de atividade

--- app/src/main/java/pt/ipg/mensagem/MainActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MainActivity.kt
@@ -16,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
         val mensagem = editTextMensagem.text.toString()
 
+        if (mensagem.isBlank()) {
+            editTextMensagem.error = "Introduza uma mensagem"
+            return
+        }
+
         val intent = Intent(this, MostraMensagemActivity::class.java)
         intent.putExtra(INFO_EXTRA_MENSAGEM, mensagem)
         startActivity(intent)
